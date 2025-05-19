0f13ba876f3095fa681d02e9fc9d7cb266a38699
appleshoops <95598768+appleshoops@users.noreply.github.com>
2025-05-20 08:19:03 +1000

    hi jake

--- John Man/game.py
+++ John Man/game.py
@@ -6,12 +6,24 @@ import math
 # create constant variables
 TILEWIDTH = 16
 TILEHEIGHT = 16
-NUMBERROWS = 36
-NUMBERCOLS = 28
+NUMBERROWS = 50
+NUMBERCOLS = 75
 SCREENWIDTH = TILEWIDTH * NUMBERCOLS
 SCREENHEIGHT = TILEHEIGHT * NUMBERROWS
 BLACK = (0, 0, 0)
 
-class GameController(obejct):
-    def __init__(self):
-        pygame.init()
+pygame.init()
+screen = pygame.display.set_mode([SCREENWIDTH, SCREENHEIGHT])
+timer = pygame.time.Clock()
+frames = 60
+
+running = True
+while running:
+    timer.tick(frames)
+    screen.fill(BLACK)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
+pygame.quit()
