b2314f2eed4119b195386faef79256d9d1180601
appleshoops <95598768+appleshoops@users.noreply.github.com>
2025-05-20 10:25:20 +1000

    dvslijdfv

--- John Man/game.py
+++ John Man/game.py
@@ -1,26 +1,36 @@
 # imports
 import pygame
 from pygame.locals import *
+from board import boards
 import math
 
 # create constant variables
 TILEWIDTH = 16
 TILEHEIGHT = 16
-NUMBERROWS = 50
-NUMBERCOLS = 75
+NUMBERROWS = 31
+NUMBERCOLS = 30
 SCREENWIDTH = TILEWIDTH * NUMBERCOLS
 SCREENHEIGHT = TILEHEIGHT * NUMBERROWS
 BLACK = (0, 0, 0)
+
+class Object():
+    def __init__(self, row, col, xPos, yPos):
+        self.__row = row
+        self.__col = col
+        self.__xPos = col * TILEWIDTH
+        self.__yPos = row * TILEHEIGHT
 
 pygame.init()
 screen = pygame.display.set_mode([SCREENWIDTH, SCREENHEIGHT])
 timer = pygame.time.Clock()
 frames = 60
+level = boards
 
 running = True
 while running:
     timer.tick(frames)
     screen.fill(BLACK)
+    draw_board(boards)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
